fcc81d423e1e16c4340313784de7f42932c206bd
jeremylevine0809 <jl506@rice.edu>
2025-07-10 11:15:20 -0500

    Committing SPI Code Trial 1

---
+++ src/main2.c
@@ -0,0 +1,34 @@
+#include <zephyr/kernel.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
+#include <zephyr/drivers/gpio.h>
+#include <zephyr/sys/printk.h>
+
+const struct device *port1 = DEVICE_DT_GET(DT_NODELABEL(gpio1));
+
+void main(void)
+{
+    if (!device_is_ready(port1)) {
+        printk("Port 1 not ready\n");
+        return;
+    }
+
+    for (int pin = 8; pin <= 14; pin++) {
+        int ret;
+
+        // Configure pin as output, initially HIGH
+        ret = gpio_pin_configure(port1, pin, GPIO_OUTPUT);
+        gpio_pin_set(port1, pin, 1);
+        k_sleep(K_MSEC(1));
+        if (ret != 0) {
+            printk("Failed to configure P1.%02d\n", pin);
+        }
+        int level = gpio_pin_get(port1, pin);
+        printk("Pin %d = %d\n", pin, level); // Should print 1
+    }
+        
+
+    while (1) {
+        k_sleep(K_MSEC(1000));
+    }
+}
